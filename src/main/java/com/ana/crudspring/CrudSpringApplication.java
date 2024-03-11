3a7118931a9d857cd1c7ee9be30fc0e36ee1077f
ana-msilva <ana.msilva@ibm.com>
2024-03-11 11:13:08 -0300

    feat: Listar Cursos (API HTTP GET)

--- src/main/java/com/ana/crudspring/CrudSpringApplication.java
+++ src/main/java/com/ana/crudspring/CrudSpringApplication.java
@@ -1,13 +1,31 @@
 package com.ana.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.ana.crudspring.model.Course;
+import com.ana.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository){
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("Front-end");
+
+			courseRepository.save(c);
+		};
 	}
 
 }
